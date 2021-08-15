fe6362d22e2148e81eed2bf4ceb462b3c7eaaac9
Nico Ramos <nico.mramos@gmail.com>
2021-08-15 03:43:49 -0300

    RandomChallenge
    RandomChallenge

---
+++ RandomChallenge/BeatifulDays/src/main/java/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(beatifullDays(13, 45, 3));
+    }
+
+    public static int beatifullDays(int i, int j, int k) {
+        if (i < 0 || j < 0 || j < i) {
+            return -1;
+        }
+
+        int count = 0;
+        for (int l = i; l <= j; l++) {
+            int diff = l - reverse(l);
+
+            if (diff % k == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static int reverse(int i) {
+        StringBuilder str = new StringBuilder(String.valueOf(i));
+        return Integer.parseInt(str.reverse().toString());
+    }
+}
